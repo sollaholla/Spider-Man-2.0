dc0c2f45d6884a0c49cbbc7428605c5cf25210c4
Soloman N <sxnorthrop15@gmail.com>
2017-11-28 14:19:48 -0500

    Comment Code

--- spiderman.net/Abilities/Attributes/WebTechAttribute.cs
+++ spiderman.net/Abilities/Attributes/WebTechAttribute.cs
@@ -4,12 +4,21 @@ namespace spiderman.net.Abilities.Attributes
 {
     public class WebTechAttribute : Attribute
     {
+        /// <summary>
+        /// The main ctor.
+        /// </summary>
+        /// <param name="categoryName">The name of the category for this web tech.</param>
         public WebTechAttribute(string categoryName)
         {
             CategoryName = categoryName;
         }
 
         public string CategoryName { get; }
+
+        /// <summary>
+        /// Set to true if you wish for this ability to be
+        /// the default ability for the category.
+        /// </summary>
         public bool IsDefault { get; set; }
     }
 }
